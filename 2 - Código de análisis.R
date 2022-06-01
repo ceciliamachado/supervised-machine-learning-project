2ddb9749a7b6d548a9f6a8a79c65108d3dc3994a
ceciliamachado <cecimachado8@gmail.com>
2022-06-01 01:18:43 -0300

    Update Part 2

--- 2 - Código de análisis.R
+++ 2 - Código de análisis.R
@@ -42,4 +42,58 @@ best_model_1 <- regsubsets(Y ~ .,
                          data = x, 
                        nvmax = x^10)
 
+#=========================================================================
+
+# Preámbulo 2 #
+
+# Borramos datos de la memoria
+rm(list = ls())
+
+# Establecemos directorio de trabajo
+setwd("C:/Users/Cecilia Machado/Desktop/Obligatorio MLS")
+
+# Cargamos libreria a utilizar
+library(MASS)
+library(ISLR)
+library(car)
+
+# Cargamos datos
+datos <-read.table("Auto.data", header = T, na.strings = "?", stringsAsFactors = T)
+
+# Visualizamos datos
+View(datos)
+dim(datos)
+names(datos)
+
+# Omitimos valores NA
+datos <- na.omit(datos)
+dim(datos)
+View(datos)
+
+# Fin del preambulo #
+#====================================================================
+
+#**********
+# Parte 2 
+#**********
+
+# Diagrama de dispersión con todas las varibles
+
+diagrama_disp <- pairs(datos)
+
+# Matriz de correlaciones 
+
+correlaciones <- cor(datos[,-9])
+correlaciones
+  
+# Regresión lineal múltiple para la variable "mpg"
+
+reg_multtiple_mpg <- lm(mpg ~ . - name, data = datos)
+summary(reg_multtiple_mpg)
+
+# Verificar fit del modelo 
+
+fit <- plot(reg_multtiple_mpg)
+
+
 
