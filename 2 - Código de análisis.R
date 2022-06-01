3c234ac8a9c4e68a4cde36226bf71dba60d58b94
ceciliamachado <cecimachado8@gmail.com>
2022-05-31 22:04:40 -0300

    Initial commit

---
+++ 2 - Código de análisis.R
@@ -0,0 +1,45 @@
+##########################################################################
+############--------------Universidad ORT Uruguay---------------##########
+############--------Obligatorio Machine Learning Supervisado----##########
+############--------------------Cecilia Machado-----------------##########
+############----------------Prof. Santiago Acerenza-------------##########
+##########################################################################
+
+#=========================================================================
+
+# Preámbulo 1 #
+
+# Borramos datos de la memoria
+rm(list = ls())
+
+# Establecemos directorio de trabajo
+setwd("C:/Users/Cecilia Machado/Desktop/Obligatorio MLS")
+
+# Cargamos libreria a utilizar
+
+
+# Cargamos los datos
+datos = Hitters
+
+## Visualizamos los datos 
+View(datos)
+
+# 
+summary(datos)
+
+# Fin del preambulo #
+#====================================================================
+
+#**********
+# Parte 1 
+#**********
+
+#**************
+# Modelo lineal 
+#**************
+
+#**************************
+# Regularización de métodos 
+#**************************
+
+
